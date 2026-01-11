e12ccb084fea1352968c68f14e0a2502c5cf7cd0
Melcat <luis.simental.itid@unipolidgo.edu.mx>
2026-01-11 17:08:31 -0600

    Refactor bot setup and add DISCORD_TOKEN check
    Updated bot initialization and added token check.

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
+import os
 import discord
 from discord.ext import commands
-import threading
-import os
-
-class DashboardBot(commands.Bot):
-    async def setup_hook(self):
-        await self.load_extension("cogs.moodle_calendar")
-
 
 intents = discord.Intents.default()
-bot = DashboardBot(command_prefix="!", intents=intents)
+intents.message_content = True  # NECESARIO
 
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"✅ Bot conectado como {bot.user}")
+    print(f"Bot conectado como {bot.user}")
 
-def run_bot():
-    bot.run(os.environ["DISCORD_TOKEN"])
+TOKEN = os.environ.get("DISCORD_TOKEN")
 
+if not TOKEN:
+    raise RuntimeError("DISCORD_TOKEN no existe")
 
+bot.run(TOKEN)
 
