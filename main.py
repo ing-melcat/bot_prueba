6dea044a3f8aaad9dd666dd4ff36eefaf2cd8efb
Melcat <luis.simental.itid@unipolidgo.edu.mx>
2026-01-11 17:21:59 -0600

    Implement health check and load cogs
    Added health check server and manual cog loading.

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 import os
+import threading
 import discord
 from discord.ext import commands
 
-intents = discord.Intents.default()
-intents.message_content = True  # NECESARIO
+# Health check
+from health import start_health_server
 
+# Intents
+intents = discord.Intents.default()
+intents.message_content = True
+
+# Bot
 bot = commands.Bot(command_prefix="!", intents=intents)
+
+# Cargar cog manualmente
+async def load_cogs():
+    await bot.load_extension("cogs.moodle_calendar")
 
 @bot.event
 async def on_ready():
-    print(f"Bot conectado como {bot.user}")
+    print(f"✅ Bot conectado como {bot.user}")
 
-TOKEN = os.environ.get("DISCORD_TOKEN")
+# Arrancar health check en otro hilo
+threading.Thread(target=start_health_server, daemon=True).start()
 
-if not TOKEN:
-    raise RuntimeError("DISCORD_TOKEN no existe")
+# Main
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(load_cogs())  # carga el cog
+    bot.run(os.environ["DISCORD_TOKEN"])
 
-bot.run(TOKEN)
 
